3083256d26fc531c2d7e3e3257bca35618e95666
Sonali Burman <sonali.b.020200@gmail.com>
2024-06-09 02:51:35 +0530

    solved

--- bookingSystem.js
+++ bookingSystem.js
@@ -1,9 +1,12 @@
 class FacilityBookingSystem {
     constructor() {
+        // This will store all bookings
         this.bookings = {
             'Clubhouse': [],
             'Tennis Court': []
         };
+
+        // Configuration of facilities and their rates
         this.facilityRates = {
             'Clubhouse': [
                 { start: 10, end: 16, rate: 100 },
@@ -15,6 +18,7 @@ class FacilityBookingSystem {
         };
     }
 
+    // Method to check for overlap in bookings
     isOverlap(existingBookings, newStart, newEnd) {
         for (const booking of existingBookings) {
             if (newStart < booking.end && newEnd > booking.start) {
@@ -24,6 +28,7 @@ class FacilityBookingSystem {
         return false;
     }
 
+     // Method to calculate the booking amount based on the rates
     calculateBookingAmount(facility, startHour, endHour) {
         let totalAmount = 0;
         for (let hour = startHour; hour < endHour; hour++) {
@@ -37,15 +42,20 @@ class FacilityBookingSystem {
         return totalAmount;
     }
 
+    // Method to book a facility
     bookFacility(facility, date, startTime, endTime) {
         const startHour = parseInt(startTime.split(':')[0]);
         const endHour = parseInt(endTime.split(':')[0]);
 
+        // Check if the facility is already booked for the given time
         if (this.isOverlap(this.bookings[facility], startHour, endHour)) {
             return `Booking Failed, Already Booked`;
         }
 
+        // Calculate the booking amount
         const bookingAmount = this.calculateBookingAmount(facility, startHour, endHour);
+
+        // Add the new booking
         this.bookings[facility].push({ date, start: startHour, end: endHour });
 
         return `Booked, Rs. ${bookingAmount}`;
